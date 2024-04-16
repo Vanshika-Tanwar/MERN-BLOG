a347afdc12a30863e9b9d55ba526017f6258d110
Vanshika-Tanwar <vanshikatanwar04.bpit@gmail.com>
2024-04-16 22:31:49 +0530

    Additional Changes

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -44,8 +44,8 @@ export default function SignUp() {
         {/* left */}
         <div className='flex-1'>
         <Link to="/" className='font-bold dark:text-white text-4xl'>
-        <span className='px-2 py-1 bg-gradient-to-r from-yellow-300 via-yellow-200 to-yellow-400 rounded-lg text-black font-semibold'>सुविचार</span>
-        Board
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white font-semibold'>Vanshika's</span>
+        Blog
       </Link>
       <p className='text-sm mt-5'>
       This is a demo project. You can sign up with your email and password or with Google. 
